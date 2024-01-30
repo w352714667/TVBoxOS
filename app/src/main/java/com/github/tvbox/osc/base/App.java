331ea4e03afba296fab44c7e2ae926094d685596
w352714667 <36921530+w352714667@users.noreply.github.com>
2024-01-30 18:48:19 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -67,6 +67,16 @@ public class App extends MultiDexApplication {
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
         }
+         }
+       if (!Hawk.contains(HawkConfig.IJK_CODEC)) {            
+           Hawk.put(HawkConfig.IJK_CODEC, "硬解码");        
+       } 
+       if (!Hawk.contains(HawkConfig.DOH_URL)) {           
+           Hawk.put(HawkConfig.DOH_URL,2);        
+       }
+      if (!Hawk.contains(HawkConfig.SEARCH_VIEW)) {            
+           Hawk.put(HawkConfig.SEARCH_VIEW, 0);        
+       }
     }
 
     public static App getInstance() {
