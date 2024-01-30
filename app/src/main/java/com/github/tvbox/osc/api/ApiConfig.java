499eab6b9a254c36cc1f70b495003d9237ce4883
w352714667 <36921530+w352714667@users.noreply.github.com>
2024-01-30 18:40:22 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitee.com/WanNenDeQunZhu/omnipotent/raw/master/db.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
